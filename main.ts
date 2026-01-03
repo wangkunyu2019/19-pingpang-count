1b6a4653c6b7972f52c0a9972dad66003165df8f
wangkunyu2019 <2019040262@qq.com>
2026-01-03 21:41:43 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Player1 += 1
+    basic.showNumber(Player1)
 })
+input.onGesture(Gesture.Shake, function () {
+    Player1 = 0
+    Player2 = 0
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Joe:")
+    basic.showNumber(Player1)
+    basic.showString("Sam:")
+    basic.showNumber(Player2)
+})
+input.onButtonPressed(Button.B, function () {
+    Player2 += 1
+    basic.showNumber(Player2)
+})
+let Player2 = 0
+let Player1 = 0
+Player1 = 0
+Player2 = 0
